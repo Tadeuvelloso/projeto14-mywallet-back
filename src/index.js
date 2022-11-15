1446ff84c78e27d79aed1876499d7f1e9b09c685
Tadeuvelloso <tadeuvellososilva@gmail.com>
2022-11-15 20:42:27 -0300

    sign-up route complete

--- src/index.js
+++ src/index.js
@@ -3,16 +3,27 @@ import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import joi from "joi";
-
-const app = express();
-
+import bcrypt from "bcrypt"
 
 // Configs
+const app = express();
 dotenv.config();
 app.use(cors());
 app.use(express.json());
 
 // Schemas
+const transactionSchema = joi.object({
+    value: joi.number().min(1).required(),
+    description: joi.string().min(3).required(),
+    type: joi.string().valid("positive", "negative").required()
+});
+
+const userSchema = joi.object({
+    name: joi.string().min(3).max(100).required(),
+    email: joi.email().required(),
+    password: joi.string().required()
+});
+
 
 // MongoDB
 const MongoClient = new MongoClient(process.env.MONGO_URI);
@@ -24,14 +35,60 @@ try {
     console.log(err)
 }
 const db = MongoClient.db("myWallet");
-const users = db.collection("users");
-const movement = db.collection("movement")
+const usersCollection = db.collection("users");
+const movementsCollection = db.collection("movement")
 
-//Rotas
+//Routes
+
+app.post("/sign-up", async (req, res) => {
+    const user = req.body;
+
+    try {
+        const userExists = await usersCollection.findOne({ email: user.email })
+
+        if (userExists) {
+            return res.status(409).send("Email já existente!");
+        }
+
+        const { error } = userSchema.validate(user, { abortEarly: false });
+
+        if (error) {
+            const errors = error.details.map(detail => detail.message);
+            return res.status(400).send(errors);
+        }
+
+        const hashPassword = bcrypt.hashSync(user.password, 10);
+
+
+        await usersCollection.insertOne({ ...user, password: hashPassword });
+        res.sendStatus(201);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+
+})
+
+app.post("/login", async (req, res) => {
+
+});
+
+app.get("/transactions", async (req, res) => {
+
+});
+
+app.post("/transactions", async (req, res) => {
+
+});
+
+// app.put("/transactions", async (req, res) => {
+
+// });
+
+// app.delete("/transactions", async (req, res) => {
+
+// });
 
 
 
-
-
-
-app.listen(4000, () => { console.log("Running app in port: 4000!")});
+app.listen(4000, () => { console.log("Running app in port: 4000!") });
