4db2c35c053163c6015ca554212f1c417b1a97e9
Tadeuvelloso <tadeuvellososilva@gmail.com>
2022-11-15 23:48:17 -0300

    sign-in with token complete

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import joi from "joi";
 import bcrypt from "bcrypt"
+import { v4 as uuidV4 } from 'uuid';
+
 
 // Configs
 const app = express();
 dotenv.config();
 app.use(cors());
 app.use(express.json());
+
 
 // Schemas
 const transactionSchema = joi.object({
@@ -37,6 +40,7 @@ try {
 const db = MongoClient.db("myWallet");
 const usersCollection = db.collection("users");
 const movementsCollection = db.collection("movement")
+const sessionsCollection = db.collection("sessions")
 
 //Routes
 
@@ -59,7 +63,6 @@ app.post("/sign-up", async (req, res) => {
 
         const hashPassword = bcrypt.hashSync(user.password, 10);
 
-
         await usersCollection.insertOne({ ...user, password: hashPassword });
         res.sendStatus(201);
     } catch (err) {
@@ -71,6 +74,7 @@ app.post("/sign-up", async (req, res) => {
 
 app.post("/sign-in", async (req, res) => {
     const {email, password} = req.body;
+    const token = uuidV4();
 
     try {
         const userExists = await usersCollection.findOne({ email });
@@ -83,17 +87,37 @@ app.post("/sign-in", async (req, res) => {
         if(!passwordValidation){
             return res.sendStatus(401);
         }
-        
 
+        await sessions.insertOne({userId: userExists._id, token})
+
+        res.send(token)
     } catch (err) {
         console.log(err);
         res.sendStatus(500);
     }
 });
 
-app.get("/transactions", async (req, res) => {
+// app.get("/transactions", async (req, res) => {
+//     const { authorization } = req.headers;
+//     const token = authorization?.replace('Bearer ', '');
+//     if(!token) {
+//         return res.sendStatus(401);
+//     }
+//     try {
+//         const sessions = await session.findOne({ token });
+//         console.log(sessions)
+//         const user = await usersCollection.findOne({ _id: sessions?.userId });
+        
+//         delete user.password;
+        
+//         res.send(user);
+//       } catch (err) {
+//         console.log(err);
+//         res.sendStatus(500);
+//       }
+// });
 
-});
+ 
 
 app.post("/transactions", async (req, res) => {
 
